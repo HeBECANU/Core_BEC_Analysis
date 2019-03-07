0cce17814195cb2224d50c4484db2f1de56b30e9
Bryce <bryce.henson@live.com>
2019-03-07 18:33:34 +1100

    merged some changed david made to the txy_importer that impoves the input flexibility/error checking

--- lib/import_mcp_tdc_data/txy_importer.m
+++ lib/import_mcp_tdc_data/txy_importer.m
@@ -1,9 +1,28 @@
-function  data=txy_importer(filepath,filenum_str)
+function  data=txy_importer(filepath,filenum)
 %this function imports txy files faster by using strongly a matched import
 %double precision
-%simply pass the filepath and file number (as string)
+%simply pass the filepath and file number
+% 31/10/16 DKS - filenum will accept integer and int string - error raised
+%   for  non-int/str filenum
+%% ERROR CHECK
+% Check filenum variable type
+if isa(filenum,'char')
+    if isempty(str2double(filenum))
+        error('filenum: %s is not an integer string',filenum);
+    else
+        filenum=str2double(filenum);
+    end
+elseif ~isa(filenum,'double')
+    % error on data type
+    error('filenum is not an int or int str');
+end
 
-fileID = fopen([filepath,'_txy_forc',filenum_str,'.txt'],'r');
+% Check filenum is an integer
+if ~(filenum==floor(filenum))
+    error('filenum: %u is not an integer - returning null data',filenum);
+end
+
+fileID = fopen([filepath,'_txy_forc',num2str(filenum),'.txt'],'r');
 data = textscan(fileID,'%f64%f64%f64',...%f32
     'Delimiter', ',', ...
     'MultipleDelimsAsOne',0,...
