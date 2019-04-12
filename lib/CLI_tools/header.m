f26fab6432e15424259dfe572afa3e72038226f8
Bryce Henson <bryce.henson@live.com>
2019-04-12 17:35:10 +1000

    header changed to cli_header & legacy function (with warn) added

--- lib/CLI_tools/header.m
+++ lib/CLI_tools/header.m
@@ -1,38 +1,8 @@
-function header(args)
+function header(vargin)
     % A function that makes nice sub headers in the console
     % Works OK with numeric substitution, but needs a length checking
     % function.
-    
-    % Examples:
-%         header('Header examples')
-%         header({2,'Some options:'})
-%         header({3,'%u %u,...',[1,2]})
-    
-    % Improvements; 
-%             switch to varargin
-%             Tests
-    vals = [];
-    if ischar(args)%If no header level is passed, assumed to be top-level
-        msg = args;
-        lvl = 0;
-    elseif iscell(args)
-        if ischar(args{1})
-            lvl = 0;
-            msg = args{1};
-            p=1;
-        else
-            lvl = args{1};
-            msg = args{2};
-            p=2;
-        end
-        if numel(args)>p
-            vals = args{p+1:end};
-        end
-    end
-
-    msg_out = sprintf(msg,vals);
-    blank = '------------------------------------------------------------';
-    marker = blank(1:10-3*lvl);
-    fprintf([marker,'   ',msg_out,'\n'])
+    warning('this function has been migrated to cli_header and will be removed in a future release')
+    cli_header(vargin)
 
 end
