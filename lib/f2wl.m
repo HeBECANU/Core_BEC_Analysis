5e09c78cbe7222c5410f0304e3445b13fffff0e8
groundhogstate <jake.ross271@gmail.com>
2019-05-17 13:13:32 +1000

    Function for wl/freq uncertainty conversion for the lazy man.

---
+++ lib/f2wl.m
@@ -0,0 +1,14 @@
+function [f,df] = f2wl(varargin)
+% Converts wavlength to frequency, and vice versa, with (absolute) uncertainty 
+% The conversion is reciprocal, so this function converts both ways.
+%   Inputs:     Wavelength (or frequency) in meters (or Hz)
+%         (opt) Uncertainty (in same units)
+%   Outputs:    Frequency (or wavelength) in Hz (or m)
+%               Uncertainty (in same units) (iff also passed as input)
+
+    c = 299798452; %Update as necessary
+    f = c./varargin{1};
+    if nargin>1
+        df = c*varargin{2}./varargin{1}.^2;
+    end
+end
