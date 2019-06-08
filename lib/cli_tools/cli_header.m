8b843028b8ef81ae84470ca2765dcccb3957306a
groundhogstate <jake.ross271@gmail.com>
2019-06-08 12:21:55 +1000

    Updated CLI_header for varargin input, no longer needs cell

--- lib/cli_tools/cli_header.m
+++ lib/cli_tools/cli_header.m
@@ -1,4 +1,4 @@
-function header(args)
+function cli_header(varargin)
     % A function that makes nice sub headers in the console
     % Works OK with numeric substitution, but needs a length checking
     % function.
@@ -12,24 +12,20 @@ function header(args)
 %             switch to varargin
 %             Tests
     vals = [];
-    if ischar(args)%If no header level is passed, assumed to be top-level
-        msg = args;
-        lvl = 0;
-    elseif iscell(args)
-        if ischar(args{1})
+    if nargin == 1 %text only
+        msg = varargin{1};
+        lvl = 0;%If no header level is passed, assumed to be top-level
+    else
+        if isfloat(varargin{1}) %Passing header level
+            lvl = varargin{1};
+            msg = varargin{2};
+            vals = varargin{3:end};
+        else % No header level passed, but other values present
             lvl = 0;
-            msg = args{1};
-            p=1;
-        else
-            lvl = args{1};
-            msg = args{2};
-            p=2;
-        end
-        if numel(args)>p
-            vals = args{p+1:end};
+            msg = varargin{1};
+            vals = varargin{2:end};
         end
     end
-
     msg_out = sprintf(msg,vals);
     blank = '------------------------------------------------------------';
     marker = blank(1:10-3*lvl);
