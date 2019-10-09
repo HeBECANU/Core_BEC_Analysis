e805d15dcd2324660fc3e217ca55f9945ede495e
groundhogstate <jake.ross271@gmail.com>
2019-10-09 22:43:30 +1100

    Merging...

--- lib/cli_tools/cli_header.m
+++ lib/cli_tools/cli_header.m
@@ -36,14 +36,13 @@ function msg_out = cli_header(varargin)
         msg_level = 0;%If no header level is passed, assumed to be top-level
         argument_index=2;
     else
-
         if isfloat(varargin{1}) %Passing header level
-            lvl = varargin{1};
+            msg_level = varargin{1};
             msg = varargin{2};
             if nargin>2
                 vals = varargin{3:end};
             end
-
+            argument_index = 1;
         else % No header level passed, but other values present
             msg_level = 0;
             msg = args{1};
