311acd56edce918645b557df033ae08532156ad6
brycehenson <bryce.henson@live.com>
2020-08-09 14:59:07 +1000

    added function to chagne number of workers for local pool

--- lib/metastable_helium_polzarizability/aprox_he_polz.m
+++ lib/metastable_helium_polzarizability/aprox_he_polz.m
@@ -22,6 +22,7 @@ atom_u=[];
 atom_u.energy=4.3597447222071e-18 ;%J
 atom_u.time=2.4188843265857e-17 ;%s
 atom_u.polz=1.64877727436e-41;% C2⋅m2⋅J−1 
+atom_u.second_hyp_polz=6.2353799905e-65;% C^4⋅m^4⋅J^-3
 atom_u.a0=5.29177210903e-11 ;% m
 
 % use the theory calculated value of the polz for states above n=3
